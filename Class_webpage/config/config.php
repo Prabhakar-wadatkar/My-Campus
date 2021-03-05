780e7b26667fb4c387467f169d41110b1822ea91
Prabhakar  Digambar Wadatkar <54213586+Prabhakar-wadatkar@users.noreply.github.com>
2021-03-05 21:40:56 +0530

    Add files via upload

---
+++ Class_webpage/config/config.php
@@ -0,0 +1,9 @@
+<?php
+define("HOSTNAME","localhost");
+define("USERNAME","root");
+define("PASSWORD","");
+define("DBNAME","user");
+$con = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DBNAME) or die ("your not connected"); 
+
+
+?>
